7562bf227fbba2848b17ede3efee080aece680ed
cesarfuster <cfuster@uol.com.br>
2018-04-04 19:11:51 -0300

    Footer Show fixed

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,7 +2,7 @@ class Instrument < ApplicationRecord
   belongs_to :user
   has_many :rents, dependent: :destroy
   mount_uploader :photo, PhotoUploader
-  validates :category, inclusion: { in: ['drums', 'guitar', 'bass', 'violin', 'xylophone', 'keyboard', 'piano', 'cello', 'microphone', 'PA']}
+  validates :category, inclusion: { in: ['drums', 'guitar', 'bass', 'violin', 'xylophone', 'keyboard', 'piano', 'cello', 'microphone']}
   validates :condition, inclusion: { in: ['new', 'used']}
   validates :day_value, presence: true
   validates :photo, presence: true

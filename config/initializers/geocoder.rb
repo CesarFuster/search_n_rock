f3a5935ea4589afcd5e4aa8e4aa569dc5be7345d
fabricio zanette <fabzanette@gmail.com>
2018-04-06 14:23:58 -0300

    fixed geocoding added autocomplete

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,8 @@
 Geocoder.configure(
   units: :km,
+  lookup: :google,
+  api_key: ENV['AIzaSyCwa8k8LvR8Ra2zNSTdOYzHKf_PvQlAsZw'],
+  use_https: true,
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
   # lookup: :google,            # name of geocoding service (symbol)

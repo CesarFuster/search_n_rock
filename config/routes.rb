56c1cb3f3a011a1b5413994a3e32d6320d119e56
fabricio zanette <fabzanette@gmail.com>
2018-04-26 11:18:07 -0300

    add facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :instruments do
     resources :rents, only: [:new, :create]
